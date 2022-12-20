c06641f0caab3f18c828a02de0b9dc9d34afde2a
TexTrue <65154269+TexBlock@users.noreply.github.com>
2022-12-20 14:50:27 +0800

    clean

--- quilt/src/main/java/com/github/tartaricacid/i18nupdatemod/quilt/mixin/MixinBookContentClasspathLoader.java
+++ quilt/src/main/java/com/github/tartaricacid/i18nupdatemod/quilt/mixin/MixinBookContentClasspathLoader.java
@@ -26,8 +26,6 @@ import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.util.Collection;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 @Mixin(value = BookContentClasspathLoader.class, remap = false)
 public class MixinBookContentClasspathLoader {
