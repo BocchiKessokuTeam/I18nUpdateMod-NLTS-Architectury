c5954b8e9dc2fa6befb4a48e7c614ef6503119c0
TexTrue <65154269+TexBlock@users.noreply.github.com>
2022-12-20 14:52:33 +0800

    clean

--- fabric/src/main/java/com/github/tartaricacid/i18nupdatemod/fabric/I18nUpdateModFabric.java
+++ fabric/src/main/java/com/github/tartaricacid/i18nupdatemod/fabric/I18nUpdateModFabric.java
@@ -3,7 +3,6 @@ package com.github.tartaricacid.i18nupdatemod.fabric;
 import com.github.tartaricacid.i18nupdatemod.I18nUpdateMod;
 import net.fabricmc.api.ModInitializer;
 public class I18nUpdateModFabric implements ModInitializer {
-
     @Override
     public void onInitialize() {
         I18nUpdateMod.init();
