02f4a61e23efabfbd2b496d3fe8fc1b9814944c2
TexTrue <65154269+TexBlock@users.noreply.github.com>
2022-12-22 13:38:50 +0800

    fix

--- fabric/src/main/java/com/github/tartaricacid/i18nupdatemod/fabric/I18nUpdateModExpectPlatformImpl.java
+++ fabric/src/main/java/com/github/tartaricacid/i18nupdatemod/fabric/I18nUpdateModExpectPlatformImpl.java
@@ -1,6 +1,5 @@
 package com.github.tartaricacid.i18nupdatemod.fabric;
 
-import com.github.tartaricacid.i18nupdatemod.I18nUpdateMod;
 import com.github.tartaricacid.i18nupdatemod.I18nUpdateModExpectPlatform;
 import net.fabricmc.loader.api.FabricLoader;
 
@@ -19,8 +18,8 @@ public class I18nUpdateModExpectPlatformImpl {
     public static String isMD5Link() {
         return "http://downloader1.meitangdehulu.com:22943/1.18-fabric.md5";
     }
-    public static Path isMD5Path() {
-        return I18nUpdateMod.CACHE_DIR.resolve("1.18-fabric.md5");
+    public static String isMD5Path() {
+        return "1.18.md5";
     }
     public static String isPackName() {
         return "Minecraft-Mod-Language-Modpack-1-18-Fabric.zip";
