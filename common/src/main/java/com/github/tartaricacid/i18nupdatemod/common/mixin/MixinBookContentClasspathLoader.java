cfdd3ed3e39182e4a895786aa0367074faa06392
TexTrue <65154269+TexBlock@users.noreply.github.com>
2022-12-23 09:38:24 +0800

    fix

--- common/src/main/java/com/github/tartaricacid/i18nupdatemod/common/mixin/MixinBookContentClasspathLoader.java
+++ common/src/main/java/com/github/tartaricacid/i18nupdatemod/common/mixin/MixinBookContentClasspathLoader.java
@@ -6,11 +6,16 @@ package com.github.tartaricacid.i18nupdatemod.common.mixin;
 import com.github.tartaricacid.i18nupdatemod.I18nUpdateMod;
 import com.google.common.base.Preconditions;
 import com.google.gson.JsonElement;
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Stream;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.packs.resources.Resource;
-import net.minecraft.server.packs.resources.ResourceManager;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.util.Identifier;
+import net.minecraft.resource.ResourceManager;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -19,57 +24,46 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import vazkii.patchouli.client.book.BookContentClasspathLoader;
 import vazkii.patchouli.client.book.BookContentLoader;
-import vazkii.patchouli.client.book.BookContentsBuilder;
 import vazkii.patchouli.common.book.Book;
-import vazkii.patchouli.common.book.BookRegistry;
 
-import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.util.Collection;
-import java.util.List;
-
-@Mixin(value = BookContentClasspathLoader.class, remap = false)
+@Mixin(value = {BookContentClasspathLoader.class}, remap = false)
 public class MixinBookContentClasspathLoader {
-    @Inject(at = @At("HEAD"), method = "findFiles")
-    private void findFiles(Book book, String dir, List<ResourceLocation> list, CallbackInfo ci) {
-        String prefix = String.format("%s/%s/%s/%s", BookRegistry.BOOKS_LOCATION, book.id.getPath(), BookContentsBuilder.DEFAULT_LANG, dir);
-        Collection<ResourceLocation> files = Minecraft.getInstance().getResourceManager().listResources(prefix, p -> p.endsWith(".json"));
+    @Inject(at = {@At("HEAD")}, method = {"findFiles"}, cancellable = true)
+    private void findFiles(Book book, String dir, List<Identifier> list, CallbackInfo ci) {
+        String prefix = String.format("%s/%s/%s/%s", "patchouli_books", book.id.getPath(), "en_us", dir);
+        Collection<Identifier> files = MinecraftClient.getInstance().getResourceManager().findResources(prefix, (p) -> p.endsWith(".json"));
+        Stream<Identifier> var10000 = files.stream().distinct().filter((file) -> file.getNamespace().equals(book.id.getNamespace())).map((file) -> {
+            Preconditions.checkArgument(file.getPath().startsWith(prefix));
+            Preconditions.checkArgument(file.getPath().endsWith(".json"));
+            String newPath = file.getPath().substring(prefix.length(), file.getPath().length() - ".json".length());
+            if (newPath.startsWith("/")) {
+                newPath = newPath.substring(1);
+            }
 
-        files.stream()
-                .distinct()
-                .filter(file -> file.getNamespace().equals(book.id.getNamespace()))
-                .map(file -> {
-                    // caller expects list to contain logical id's, not file paths.
-                    // we end up going from path -> id -> back to path, but it's okay as a transitional measure
-                    Preconditions.checkArgument(file.getPath().startsWith(prefix));
-                    Preconditions.checkArgument(file.getPath().endsWith(".json"));
-                    String newPath = file.getPath().substring(prefix.length(), file.getPath().length() - ".json".length());
-                    // Vanilla expects `prefix` above to not have a trailing slash, so we
-                    // have to remove it ourselves from the path
-                    if (newPath.startsWith("/")) {
-                        newPath = newPath.substring(1);
-                    }
-                    return new ResourceLocation(file.getNamespace(), newPath);
-                })
-                .forEach(list::add);
+            return new Identifier(file.getNamespace(), newPath);
+        });
+        Objects.requireNonNull(list);
+        var10000.forEach(list::add);
+        if (!list.isEmpty()) {
+            ci.cancel();
+        }
 
     }
 
-    @Inject(at = @At("HEAD"), method = "loadJson", cancellable = true, remap = false)
-    private void loadJson(Book book, ResourceLocation resloc, @Nullable ResourceLocation fallback, CallbackInfoReturnable<JsonElement> callback) {
+    @Inject(at = {@At("HEAD")}, method = {"loadJson"}, cancellable = true, remap = false)
+    private void loadJson(Book book, Identifier resloc, @Nullable Identifier fallback, CallbackInfoReturnable<JsonElement> callback) {
         I18nUpdateMod.LOGGER.debug("[Patchouli] Loading {}", resloc);
-        ResourceManager manager = Minecraft.getInstance().getResourceManager();
-        try {
-            Resource resource = manager.getResource(resloc);
+        ResourceManager manager = MinecraftClient.getInstance().getResourceManager();
 
-            if (resource != null) {
-                callback.setReturnValue(BookContentLoader.streamToJson(resource.getInputStream()));
-            } else if (fallback != null) {
-                Resource fallbackResource = manager.getResource(fallback);
-                callback.setReturnValue(BookContentLoader.streamToJson(fallbackResource.getInputStream()));
+        try {
+            if (manager.containsResource(resloc)) {
+                callback.setReturnValue(BookContentLoader.streamToJson(manager.getResource(resloc).getInputStream()));
+            } else if (fallback != null && manager.containsResource(fallback)) {
+                callback.setReturnValue(BookContentLoader.streamToJson(manager.getResource(fallback).getInputStream()));
             }
-        } catch (IOException ex) {
-            throw new UncheckedIOException(ex);
+
+        } catch (IOException var7) {
+            throw new UncheckedIOException(var7);
         }
     }
 }
