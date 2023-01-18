ed361c620b17ae68450cb9efe7dd5aa77e6a5af2
TexTrue <65154269+TexBlock@users.noreply.github.com>
2023-01-18 09:24:00 +0800

    更改下载链接

--- quilt/src/main/java/com/github/tartaricacid/i18nupdatemod/quilt/I18nModConfigQuilt.java
+++ quilt/src/main/java/com/github/tartaricacid/i18nupdatemod/quilt/I18nModConfigQuilt.java
@@ -6,7 +6,7 @@ import me.shedaniel.autoconfig.annotation.Config;
 
 @Config(name = I18nUpdateMod.MOD_ID)
 public class I18nModConfigQuilt implements ConfigData {
-    public static String downloadLink = "https://gitcode.net/chearlai/translationpackconvert/-/raw/main/files";
-    public static String resourcePackName = "Minecraft-Mod-Language-Modpack-1-18-FabricT1-19-1o2-Fabric";
-    public static String md5Name = "1.18-fabrict1.19.1o2-fabric";
+    public static String downloadLink = "https://gitcode.net/chearlai/translationpackmirror/-/raw/main/files";
+    public static String resourcePackName = "Minecraft-Mod-Language-Modpack-1-19";
+    public static String md5Name = "1.19";
 }
